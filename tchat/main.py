93d6fb51e1d3715b28ffd21bb526ab9fed01f507
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-12-04 18:50:57 +0300

    ConversationSummaryMemory -- Summarizations

--- tchat/main.py
+++ tchat/main.py
@@ -1,18 +1,20 @@
 from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import ConversationSummaryMemory, FileChatMessageHistory
 from dotenv import load_dotenv
 
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(verbose=True)   
 
     # memory set up
-memory = ConversationBufferMemory(
-    chat_memory=FileChatMessageHistory("messages.json"),
+memory = ConversationSummaryMemory(
+    # chat_memory=FileChatMessageHistory("messages.json"),
     memory_key="messages", 
-    return_messages=True)  
+    return_messages=True,
+    llm=chat
+    )  
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "messages"], 
@@ -25,7 +27,8 @@ prompt = ChatPromptTemplate(
 chain = LLMChain(
     llm=chat, 
     prompt=prompt,
-    memory=memory      #wire up memory with the chat
+    memory=memory,      #wire up memory with the chat
+    verbose=True        # adding debug flag to model
 )
 while True: 
     content = input(">> ")
