9b30097d5af68b9a224d327aff867e1f3d0baf34
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-12-04 16:59:59 +0300

    store conversations using ChatBufferMemory

--- tchat/main.py
+++ tchat/main.py
@@ -1,25 +1,31 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import MessagesPlaceholder, HumanMessagePromptTemplate, ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
 
+    # memory set up
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)  
+
 prompt = ChatPromptTemplate(
-    input_variables=["content"], 
+    input_variables=["content", "messages"], 
     messages=[
+        MessagesPlaceholder(variable_name="messages"),
         HumanMessagePromptTemplate.from_template("{content}")
     ]
 )
 
 chain = LLMChain(
     llm=chat, 
-    prompt=prompt
+    prompt=prompt,
+    memory=memory      #wire up memory with the chat
 )
 while True: 
     content = input(">> ")
 
     result = chain({"content": content})
-    print(f"You entered: {content}")
+    print(result["text"])
