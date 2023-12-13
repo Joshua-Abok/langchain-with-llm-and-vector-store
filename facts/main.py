781c581667c496015453324530b8f57040a0d5ef
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-12-13 12:23:30 +0300

    embeds in chromadb

--- facts/main.py
+++ facts/main.py
@@ -1,8 +1,16 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+# emb = embeddings.embed_query("hi there")
+
+# print(len(emb))
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
@@ -15,6 +23,21 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs: 
-    print(doc.page_content)
+    # create a new chroma db & store all the embeddings for our docs inside it 
+db = Chroma.from_documents(
+    docs,      # creating an instance for chroma to create embed for the "docs"
+    embedding=embeddings, 
+    persist_directory="emb"  # embeds stored in sqlite db inside the emb dir
+)
+            
+            # similarity_search_with_score
+results = db.similarity_search(
+    "What is an interesting fact about the english language?"
+    # k=1   # most relevant for four results
+                                           )
+
+for result in results: 
     print("\n")
+    # print(result[1])
+    # print(result[0].page_content)
+    print(result.page_content) 
