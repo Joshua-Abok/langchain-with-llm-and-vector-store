60c833a678f5f77dfa3338e6211654a08f37f62e
Joshua Abok <nyajuaya.j.a@gmail.com>
2023-12-13 16:29:48 +0300

    chain_types - stuff, map_reduce, map_rerank & refine

--- facts/prompt.py
+++ facts/prompt.py
@@ -3,7 +3,9 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
+import langchain
 
+langchain.debug = True
 
 load_dotenv()
 
@@ -25,7 +27,10 @@ retriever = db.as_retriever()
 chain = RetrievalQA.from_chain_type(
     llm=chat, 
     retriever=retriever, 
-    chain_type="stuff" #take some context from the vector store & "stuff" it into the prompt
+    chain_type="refine" #    -> build an initial response, then give the LLM an opport. to update it with further context 
+            #   "map_reduce" -> build a summary of each doc, then feed each summary to final qn
+            #   "stuff"      -> take some context from the vector store & "stuff" it into the prompt
+            #   "map_rerank" -> find relevant part of each doc & give it a score of how relevant it is  
 )
 
 result = chain.run("What is an interesting fact about the English language")
